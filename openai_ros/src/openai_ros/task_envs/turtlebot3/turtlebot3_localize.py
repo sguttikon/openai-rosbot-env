95d454db5780121e11474fcf3018cc2c07cd1665
suresh-guttikonda <guttikondasuresh@gmail.com>
2020-10-09 14:14:20 +0200

    code refactoring

--- openai_ros/src/openai_ros/task_envs/turtlebot3/turtlebot3_localize.py
+++ openai_ros/src/openai_ros/task_envs/turtlebot3/turtlebot3_localize.py
@@ -2,8 +2,10 @@
 
 import rospy
 from openai_ros.robot_envs import turtlebot3_env
+from openai_ros.utils import Map, Pose
 from gym import spaces
 from geometry_msgs.msg import PoseArray, PoseWithCovarianceStamped
+from gazebo_msgs.msg import ModelStates
 from std_srvs.srv import Empty
 from nav_msgs.srv import GetMap
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
@@ -14,26 +16,6 @@ from matplotlib.patches import Wedge
 from matplotlib.patches import Ellipse
 from matplotlib import transforms
 import numpy as np
-
-class Map():
-    """
-        Map class is an implementation to store map details
-    """
-
-    def __init__(self):
-        """
-        Initialize Map class
-        """
-        super(Map, self).__init__()
-
-        self.origin_x = 0.0
-        self.origin_y = 0.0
-        self.scale = 0.0
-
-        self.size_x = 0
-        self.size_y = 0
-
-        self.grid_cells = None
 
 class TurtleBot3LocalizeEnv(turtlebot3_env.TurtleBot3Env):
     """
@@ -60,6 +42,9 @@ class TurtleBot3LocalizeEnv(turtlebot3_env.TurtleBot3Env):
         self._angular_speed = 0.3
 
         self._is_new_map = False
+        self._robot_radius = 3.0
+        # all sensor data, topic messages is assumed to be in same tf frame
+        self._global_frame_id = 'map'
 
         # code realted to sensors
         self._request_map = True
@@ -90,22 +75,21 @@ class TurtleBot3LocalizeEnv(turtlebot3_env.TurtleBot3Env):
 
         if self._map_data is not None:
             # environment map
-            self._draw_map(self._map_data)
+            self.__draw_map(self._map_data)
             # groundtruth pose
             self._gt_pose_plt, self._gt_heading_plt = \
-                self._draw_robot_pose(self._gazebo_pose,
+                self.__draw_robot_pose(self._gazebo_pose,
                                       self._gt_pose_plt,
                                       self._gt_heading_plt, 'blue')
             # amcl pose
             self._amcl_pose_plt, self._amcl_heading_plt = \
-                self._draw_robot_pose(self._amcl_pose.pose.pose,
+                self.__draw_robot_pose(self._amcl_pose,
                                       self._amcl_pose_plt,
                                       self._amcl_heading_plt, 'green')
             # amcl pose covariance
             self._amcl_confidence_plt = \
-                self._draw_pose_confidence(self._amcl_pose.pose.pose,
-                                           self._amcl_pose.pose.covariance,
-                                           self._amcl_confidence_plt, 'green')
+                self.__draw_pose_confidence(self._amcl_pose,
+                                            self._amcl_confidence_plt, 'green')
 
             self._plt_ax.legend([ self._gt_pose_plt, self._amcl_pose_plt ], \
                                 [ 'gt_pose', 'amcl_pose' ])
@@ -122,157 +106,50 @@ class TurtleBot3LocalizeEnv(turtlebot3_env.TurtleBot3Env):
         plt.ioff()
         plt.show()
 
-    def _draw_map(self, map):
-        """
-        Draw environment map
-
-        Parameters
-        ----------
-        map: Map
-            map of environment
-        """
-
-        if self._is_new_map:
-            x_max = map.size_x/2 -1/(map.scale*2)
-            x_min = -x_max
-            y_max = map.size_y/2 -1/(map.scale*2)
-            y_min = -y_max
-            extent = [x_min, x_max, y_min, y_max]
-            if self._map_plt == None:
-                self._map_plt = self._plt_ax.imshow(map.grid_cells,
-                            cmap=plt.cm.binary, origin='lower', extent=extent)
-
-                self._plt_ax.grid()
-                self._plt_ax.set_xlim([x_min, x_max])
-                self._plt_ax.set_ylim([y_min, y_max])
-
-                ticks_x = np.linspace(x_min, x_max)
-                ticks_y = np.linspace(y_min, y_max)
-                self._plt_ax.set_xticks(ticks_x, ' ')
-                self._plt_ax.set_yticks(ticks_y, ' ')
-
-                self._plt_ax.set_xlabel('x coords')
-                self._plt_ax.set_ylabel('y coords')
-            else:
-                pass
-
-            self._is_new_map = False
-
-    def _draw_robot_pose(self, robot_pose, pose_plt: Wedge, heading_plt, color: str):
-        """
-        Draw robot pose
-
-        Parameters
-        ----------
-        robot_pose: geometry_msgs.msg._Pose.Pose
-            robot's pose
-        pose_plt: matplotlib.patches.Wedge
-            plot of robot position
-        heading_plt: matplotlib.lines.Line2D
-            plot of robot heading
-        color: str
-            color to render for robot
-
-        Returns
-        -------
-        pose_plt: matplotlib.patches.Wedge
-            updated plot of robot position
-        heading_plt: matplotlib.lines.Line2D
-            updated plot of robot heading
-        """
-        if robot_pose is None:
-            return
-
-        # rescale robot position
-        pose_x = robot_pose.position.x / self._map_data.scale
-        pose_y = robot_pose.position.y / self._map_data.scale
-        pose_z = robot_pose.position.z / self._map_data.scale
-        roll, pitch, yaw = euler_from_quaternion([
-                            robot_pose.orientation.x,
-                            robot_pose.orientation.y,
-                            robot_pose.orientation.z,
-                            robot_pose.orientation.w
-        ])
-
-        radius = 3
-        xdata = [pose_x, pose_x + radius*3*np.cos(yaw)]
-        ydata = [pose_y, pose_y + radius*3*np.sin(yaw)]
-
-        if pose_plt == None:
-            pose_plt = Wedge((pose_x, pose_y), radius, 0, 360, color=color, alpha=0.5)
-            heading_plt, = self._plt_ax.plot(xdata, ydata, color=color, alpha=0.5)
-            self._plt_ax.add_artist(pose_plt)
-        else:
-            pose_plt.update({'center': [pose_x, pose_y]})
-            heading_plt.update({'xdata': xdata, 'ydata': ydata})
-
-        return pose_plt, heading_plt
-
-    def _draw_pose_confidence(self, robot_pose, covariance, confidence_plt, color: str, n_std=1.0):
-        """
-        Draw confidence ellipse around the robot pose
-
-        Parameters
-        ----------
-        robot_pose: geometry_msgs.msg._Pose.Pose
-            robot's pose
-        covariance:
-            robot's pose covariance
-        confidence_plt: matplotlib.patches.Ellipse
-            plot of robot position confidence
-        color: str
-            color to render for confidence
-        n_std: float
-            number of std to determine ellipse's radius
-
-        Returns
-        -------
-        confidence_plt: matplotlib.patches.Ellipse
-            updated plot of robot position confidence
-        """
-
-        # reference  https://matplotlib.org/devdocs/gallery/statistics/confidence_ellipse.html
-        # cov_xy / np.sqrt(cov_xx * cov_yy)
-        pearson = covariance[6*0 + 1]/np.sqrt(covariance[6*0 + 0] * covariance[6*1 + 1])
-
-        # compute eigenvalues and rescale
-        ell_radius_x = np.sqrt(1 + pearson) / self._map_data.scale
-        ell_radius_y = np.sqrt(1 - pearson) / self._map_data.scale
-
-        # compute mean and std
-        scale_x = np.sqrt(covariance[6*0 + 0] / self._map_data.scale) * n_std
-        mean_x = robot_pose.position.x / self._map_data.scale
-        scale_y = np.sqrt(covariance[6*1 + 1] / self._map_data.scale) * n_std
-        mean_y = robot_pose.position.y / self._map_data.scale
-
-        transform = transforms.Affine2D().rotate_deg(45) \
-                                         .scale(scale_x, scale_y) \
-                                         .translate(mean_x, mean_y)
-        if confidence_plt == None:
-            confidence_plt = Ellipse((0, 0), width=ell_radius_x, height=ell_radius_y,
-                                     facecolor='none', edgecolor=color)
-            confidence_plt.set_transform(transform + self._plt_ax.transData)
-            self._plt_ax.add_artist(confidence_plt)
-        else:
-            confidence_plt.width = ell_radius_x
-            confidence_plt.height = ell_radius_y
-            confidence_plt.set_transform(transform + self._plt_ax.transData)
-
-        return confidence_plt
-
     def _check_amcl_data_is_ready(self):
         """
         Checks amcl topics are operational
         """
+
         topic_name = '/particlecloud'
         topic_class = PoseArray
         time_out = 5.0
-        self._particle_cloud = self._check_topic_data_is_ready(topic_name, topic_class, time_out)
+        particle_msg = self._check_topic_data_is_ready(topic_name, topic_class, time_out)
+
+        if particle_msg.header.frame_id != self._global_frame_id:
+            rospy.logwarn('received amcl particle cloud must be in the global frame')
+
+        self._particle_cloud = self.__process_particle_msg(particle_msg)
 
         topic_name = '/amcl_pose'
         topic_class = PoseWithCovarianceStamped
         time_out = 1.0
-        self._amcl_pose = self._check_topic_data_is_ready(topic_name, topic_class, time_out)
+        pose_msg = self._check_topic_data_is_ready(topic_name, topic_class, time_out)
+
+        if pose_msg.header.frame_id != self._global_frame_id:
+            rospy.logwarn('received amcl pose must be in the global frame')
+
+        self._amcl_pose = self.__process_pose_cov_msg(pose_msg.pose)
+
+    def _check_gazebo_data_is_ready(self):
+        """
+        Checks gazebo topic is operational
+        """
+
+        topic_name = '/gazebo/model_states'
+        topic_class = ModelStates
+        time_out = 1.0
+        data = self._check_topic_data_is_ready(topic_name, topic_class, time_out)
+
+        # TODO: do we also need twist (velocity) of turtlebot ??
+        # preprocess received data
+        if data is not None:
+            rosbot_name = 'turtlebot3'
+            if rosbot_name in data.name:
+                turtlebot_idx = data.name.index(rosbot_name)
+                self._gazebo_pose = self.__process_pose_msg(data.pose[turtlebot_idx])
+            else:
+                rospy.logwarn('cannot retrieve ground truth pose')
 
     def _check_init_pose_pub_ready(self):
         """
@@ -289,22 +166,10 @@ class TurtleBot3LocalizeEnv(turtlebot3_env.TurtleBot3Env):
         service_class = GetMap
         msg = self._call_service(service_name, service_class)
 
-        # initialize map
-        map = Map()
-        map.scale = msg.map.info.resolution
-        map.size_x = msg.map.info.width
-        map.size_y = msg.map.info.height
+        if msg.map.header.frame_id != self._global_frame_id:
+            rospy.logwarn('received map must be in the global frame')
 
-        # rescale the map origin coordinates
-        map.origin_x = msg.map.info.origin.position.x + (map.size_x/2) * map.scale
-        map.origin_y = msg.map.info.origin.position.y + (map.size_y/2) * map.scale
-
-        map.grid_cells = np.array(msg.map.data).reshape(map.size_y, map.size_x)
-
-        self._map_data = map
-
-        self._is_new_map = True
-        self._request_map = False
+        self._map_data = self.__process_map_msg(msg.map)
 
     def _init_amcl(self, is_global=True):
         """
@@ -435,3 +300,206 @@ class TurtleBot3LocalizeEnv(turtlebot3_env.TurtleBot3Env):
 
         self._move_base( linear_speed, angular_speed,
                          self._motion_error, self._update_rate )
+
+    ###### private methods ######
+
+    def __draw_map(self, map):
+        """
+        Draw environment map
+
+        :params utils.Map map: map of robot's environment
+        """
+
+        if self._is_new_map:
+            width, height = map.get_size()
+            scale = map.get_scale()
+
+            # offset the limit the range of plot to display
+            x_max = (width/2) * (1-scale)
+            x_min = -(width/2) * (1+scale)
+            y_max = (height/2) * (1-scale)
+            y_min = -(height/2) * (1+scale)
+            extent = [x_min, x_max, y_min, y_max]
+
+            if self._map_plt == None:
+                self._map_plt = self._plt_ax.imshow(map.get_cells(),
+                            cmap=plt.cm.binary, origin='lower', extent=extent)
+
+                self._plt_ax.grid()
+                self._plt_ax.set_xlim([x_min, x_max])
+                self._plt_ax.set_ylim([y_min, y_max])
+
+                ticks_x = np.linspace(x_min, x_max)
+                ticks_y = np.linspace(y_min, y_max)
+                self._plt_ax.set_xticks(ticks_x, ' ')
+                self._plt_ax.set_yticks(ticks_y, ' ')
+
+                self._plt_ax.set_xlabel('x coords')
+                self._plt_ax.set_ylabel('y coords')
+            else:
+                pass
+
+            self._is_new_map = False
+
+    def __draw_robot_pose(self, robot_pose, pose_plt: Wedge, heading_plt, color: str):
+        """
+        Draw robot pose
+
+        :params utils.Pose robot_pose: robot's pose
+                matplotlib.patches.Wedge pose_plt: plot of robot position
+                matplotlib.lines.Line2D heading_plt: plot of robot heading
+                str color: color used to render robot position and heading
+
+        :return matplotlib.patches.Wedge, matplotlib.lines.Line2D
+        """
+        if robot_pose is None:
+            return
+
+        # rescale robot position
+        pose_x, pose_y, _ = robot_pose.get_position()
+        pose_x = pose_x / self._map_data.get_scale()
+        pose_y = pose_y / self._map_data.get_scale()
+        _, _, yaw = robot_pose.get_euler()
+
+        line_len = 3.0
+        xdata = [pose_x, pose_x + self._robot_radius* line_len * np.cos(yaw)]
+        ydata = [pose_y, pose_y + self._robot_radius* line_len * np.sin(yaw)]
+
+        if pose_plt == None:
+            pose_plt = Wedge((pose_x, pose_y), self._robot_radius, 0, 360, color=color, alpha=0.5)
+            heading_plt, = self._plt_ax.plot(xdata, ydata, color=color, alpha=0.5)
+            self._plt_ax.add_artist(pose_plt)
+        else:
+            pose_plt.update({'center': [pose_x, pose_y]})
+            heading_plt.update({'xdata': xdata, 'ydata': ydata})
+
+        return pose_plt, heading_plt
+
+    def __draw_pose_confidence(self, robot_pose, confidence_plt, color: str, n_std=1.0):
+        """
+        Draw confidence ellipse around the robot pose
+
+        :params utils.Pose robot_pose: robot's pose
+                matplotlib.patches.Wedge confidence_plt: plot of robot position confidence
+                str color: color used to render robot position confidence
+                float n_std: number of std to determine ellipse's radius
+
+        :return matplotlib.patches.Ellipse
+        """
+
+        pose_x, pose_y, _ = robot_pose.get_position()
+        covariance = robot_pose.get_covariance()
+        scale = self._map_data.get_scale()
+        # reference  https://matplotlib.org/devdocs/gallery/statistics/confidence_ellipse.html
+        # cov_xy / np.sqrt(cov_xx * cov_yy)
+        pearson = covariance[0, 1]/np.sqrt(covariance[0, 0] * covariance[1, 1])
+
+        # compute eigenvalues and rescale
+        ell_radius_x = np.sqrt(1 + pearson) / scale
+        ell_radius_y = np.sqrt(1 - pearson) / scale
+
+        # compute mean and std
+        scale_x = np.sqrt(covariance[0, 0] / scale) * n_std
+        mean_x = pose_x / scale
+        scale_y = np.sqrt(covariance[1, 1] / scale) * n_std
+        mean_y = pose_y / scale
+
+        transform = transforms.Affine2D().rotate_deg(45) \
+                                         .scale(scale_x, scale_y) \
+                                         .translate(mean_x, mean_y)
+        if confidence_plt == None:
+            confidence_plt = Ellipse((0, 0), width=ell_radius_x, height=ell_radius_y,
+                                     facecolor='none', edgecolor=color)
+            confidence_plt.set_transform(transform + self._plt_ax.transData)
+            self._plt_ax.add_artist(confidence_plt)
+        else:
+            confidence_plt.width = ell_radius_x
+            confidence_plt.height = ell_radius_y
+            confidence_plt.set_transform(transform + self._plt_ax.transData)
+
+        return confidence_plt
+
+    def __process_particle_msg(self, particle_msg):
+        """
+        Process the particle cloud message
+
+        :param
+        :return
+        """
+
+        return particle_msg
+
+    def __process_pose_cov_msg(self, pose_cov_msg):
+        """
+        Process the received pose message
+
+        :param geometry_msgs.msg._PoseWithCovariance.PoseWithCovariance pose_cov_msg: pose with covariance message
+        :return utils.Pose
+        """
+
+        # initialize pose
+        pose = self.__process_pose_msg(pose_cov_msg.pose)
+        # initialize covariance
+        pose.set_covariance(
+            pose_cov_msg.covariance
+        )
+
+        return pose
+
+    def __process_pose_msg(self, pose_msg):
+        """
+        Process the received pose message
+
+        :param geometry_msgs.msg._Pose.Pose pose_msg: pose message
+        :return utils.Pose
+        """
+
+        # initialize pose
+        pose = Pose()
+        pose.set_position(
+            pose_msg.position.x,
+            pose_msg.position.y,
+            pose_msg.position.z
+        )
+        pose.set_quaternion(
+            pose_msg.orientation.x,
+            pose_msg.orientation.y,
+            pose_msg.orientation.z,
+            pose_msg.orientation.w
+        )
+        return pose
+
+    def __process_map_msg(self, msg_map):
+        """
+        Process the received map message
+
+        :param nav_msgs.msg._OccupancyGrid.OccupancyGrid msg_map: map message
+        :return utils.Map
+        """
+
+        # initialize map
+        map = Map()
+        map.set_scale(msg_map.info.resolution)
+        map.set_size(msg_map.info.width, msg_map.info.height)
+        map.set_origin(self.__process_pose_msg(msg_map.info.origin))
+
+        # rescale and shift the map origin to world coordinates
+        origin = map.get_origin()
+        x, y, z = origin.get_position()
+        width, height = map.get_size()
+        scale = map.get_scale()
+        origin.set_position(
+                x + (width/2) * scale,
+                y + (height/2) * scale,
+                z
+        )
+
+        # set grid cells
+        map.set_cells(msg_map.data)
+
+        self._is_new_map = True
+        self._request_map = False
+
+        return map
+
+    ###### private methods ######
